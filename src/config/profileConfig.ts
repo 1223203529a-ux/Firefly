1de32b3a92e1f47437dcd0647a74c7e1e6d88005
1223203529a-ux <1223203529a@gmail.com>
2026-07-13 21:54:38 +0800

    Match Firefly QQ profile link style

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -22,10 +22,10 @@ export const profileConfig: ProfileConfig = {
 	// showName: true 时显示图标和名称，false 时只显示图标
 	links: [
 		{
-			name: "QQ",
+			name: "qq",
 			icon: "fa7-brands:qq",
 			url: "/assets/images/image.png",
-			showName: true,
+			showName: false,
 		},
 		{
 			name: "GitHub",
