05c4d38f24cbd071823f29e1b424f668d65b5116
Jing Yuan <1223203529a@gmail.com>
2026-07-14 01:28:28 +0800

    restore full profileConfig with QQ chat URL for 3651055492

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -1,1 +1,26 @@
-https://wpa.qq.com/msgrd?v=3&uin=3651055492&site=qq&menu=yes
+import type { ProfileConfig } from "@src/config";
+
+const profileConfig: ProfileConfig = {
+  name: "Firefly",
+  avatar: "/assets/images/avatar.png",
+  bio: "Hello, I'm Firefly.",
+  links: [
+    {
+      name: "qq",
+      icon: "fa7-brands:qq",
+      url: "https://wpa.qq.com/msgrd?v=3&uin=3651055492&site=qq&menu=yes",
+    },
+    {
+      name: "github",
+      icon: "fa6-brands:github",
+      url: "https://github.com/1223203529a-ux",
+    },
+    {
+      name: "email",
+      icon: "fa6-solid:envelope",
+      url: "mailto:3651055492@qq.com",
+    },
+  ],
+};
+
+export default profileConfig;
