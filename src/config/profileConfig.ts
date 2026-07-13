f20449cc39243cc5c958ac1555cd4ff483dff274
Jing Yuan <1223203529a@gmail.com>
2026-07-14 02:09:44 +0800

    Refactor profileConfig structure and add showName
    Reordered properties in profileConfig for consistency and added 'showName' property to links.

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -1,24 +1,27 @@
-import type { ProfileConfig } from "@src/config";
+import type { ProfileConfig } from "../types/profileConfig";
 
 export const profileConfig: ProfileConfig = {
-  name: "Firefly",
-  avatar: "/assets/images/avatar.png",
-  bio: "Hello, I'm Firefly.",
-  links: [
-    {
-      name: "qq",
-      icon: "fa7-brands:qq",
-      url: "https://wpa.qq.com/msgrd?v=3&uin=3651055492&site=qq&menu=yes",
-    },
-    {
-      name: "github",
-      icon: "fa6-brands:github",
-      url: "https://github.com/1223203529a-ux",
-    },
-    {
-      name: "email",
-      icon: "fa6-solid:envelope",
-      url: "mailto:3651055492@qq.com",
-    },
-  ],
+	avatar: "/assets/images/avatar.png",
+	name: "Firefly",
+	bio: "Hello, I'm Firefly.",
+	links: [
+		{
+			name: "qq",
+			icon: "fa7-brands:qq",
+			url: "https://wpa.qq.com/msgrd?v=3&uin=3651055492&site=qq&menu=yes",
+			showName: false,
+		},
+		{
+			name: "GitHub",
+			icon: "fa7-brands:github",
+			url: "https://github.com/1223203529a-ux",
+			showName: false,
+		},
+		{
+			name: "Email",
+			icon: "fa7-solid:envelope",
+			url: "mailto:3651055492@qq.com",
+			showName: false,
+		},
+	],
 };
